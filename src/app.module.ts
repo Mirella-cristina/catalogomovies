206a96b6e8f8648ccdef670e4d693b5042307331
Mirellacristina <mirela.pereira@estudante.ifms.edu.br>
2025-11-15 17:04:42 -0300

    ....

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GeneroModule } from './modules/genero/genero.module';
-import { MoviesModule } from './modules/movies/movies.module';
+import { GeneroModule } from './modules/genre/genero.module';
+import { MoviesModule } from './modules/movie/movies.module';
 
 @Module({
   imports: [GeneroModule, MoviesModule],
