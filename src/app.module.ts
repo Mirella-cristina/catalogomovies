090016e8e0a4cca871e2a1a784cc364006cdb44a
Mirellacristina <mirela.pereira@estudante.ifms.edu.br>
2025-11-15 16:47:50 -0300

    hi modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GeneroModule } from './modules/genero/genero.module';
+import { MoviesModule } from './modules/movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [GeneroModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
